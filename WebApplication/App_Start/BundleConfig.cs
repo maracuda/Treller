304a826dce86ef457a23cdbf71e1eb2802074fa0
scalder <scalder@skbkontur.ru>
2014-12-21 20:47:58 +0500

    порция красивостей для списка

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -31,7 +31,13 @@ namespace SKBKontur.Treller.WebApplication
                 .Include("~/Content/Bootstrap/bootstrap.css")
                 .Include("~/Content/Bootstrap/bootstrap-theme.css")
                 .Include("~/Content/Colorbox/colorbox.css")
-                .Include("~/Content/site.css"));
+                .Include("~/Content/site.css")
+                .IncludeDirectory("~/Content/Layout/", "*.css"));
+
+            bundles.Add(
+                new StyleBundle("~/bundles/tasklist")
+                .IncludeDirectory("~/Content/TaskList", "*.css")
+                .IncludeDirectory("~/Content/TaskInfo", "*.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
---
+++ WebApplication/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SKBKontur.Treller.WebApplication.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToLowerString<T>(this T value)
+        {
+            return value.ToString().ToLower();
+        }
+    }
+}
