f4ac69713a93642b66c4ddbc9c5f95b1bc3b32f6
mayloe <mayloe@yandex.ru>
2015-04-05 16:40:37 +0500

    WebApplication.Feature : add safe blocks usage in treller

---
+++ WebApplication/Abstractions/DefaultExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using SKBKontur.BlocksMapping.Abstrations;
+using SKBKontur.BlocksMapping.Mappings;
+
+namespace SKBKontur.Treller.WebApplication.Abstractions
+{
+    public class DefaultExceptionHandler : IBlocksExceptionHandler
+    {
+        public string[] GetContextKeys()
+        {
+            return null;
+        }
+
+        public void HandleSubBuildException(string contextKey, BlockSubBuildException exception)
+        {
+        }
+    }
+}
