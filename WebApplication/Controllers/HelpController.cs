0d90304f22e168e6d94e432f64fc36655cceabf6
maylo <maylo@skbkontur.ru>
2016-03-29 13:26:28 +0500

    Add Help controller (administration controller)

---
+++ WebApplication/Controllers/HelpController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using SKBKontur.TaskManagerClient;
+
+namespace SKBKontur.Treller.WebApplication.Controllers
+{
+    public class HelpController : Controller
+    {
+        private readonly IBugTrackerClient bugTrackerClient;
+
+        public HelpController(IBugTrackerClient bugTrackerClient)
+        {
+            this.bugTrackerClient = bugTrackerClient;
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult DeleteAllCommentsFromConsistencyBattle()
+        {
+            var issueId = "BILLY-6280";
+            var comments = bugTrackerClient.GetComments(issueId);
+            var attechments = bugTrackerClient.GetAttachments(issueId);
+
+            foreach (var comment in comments)
+            {
+                bugTrackerClient.DeleteComment(issueId, comment.Id, true);
+            }
+
+            foreach (var attachment in attechments)
+            {
+                bugTrackerClient.DeleteAttachment(issueId, attachment.Id);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
