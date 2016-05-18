0b39f299ce7283287a9051c2d45c798470088b3a
Lesha Khvorost <hvorost@skbkontur.ru>
2016-05-18 05:33:50 +0500

    fix GitLabClient async bug

--- WebApplication/Controllers/RepositoryController.cs
+++ WebApplication/Controllers/RepositoryController.cs
@@ -15,10 +15,7 @@ namespace SKBKontur.Treller.WebApplication.Controllers
 
         public ActionResult Index()
         {
-            var oldBranchesModel = new OldBranchesModel
-            {
-                TotalNumber = 100,
-            };//oldBranchesModelBuilder.Build(TimeSpan.FromDays(30), TimeSpan.FromDays(60));
+            var oldBranchesModel = oldBranchesModelBuilder.Build(TimeSpan.FromDays(30), TimeSpan.FromDays(60));
             return View("Index", oldBranchesModel);
         }
     }
