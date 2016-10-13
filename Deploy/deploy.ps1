122cd401b88a67bf1821ca4cbe3f8bfdf76bdbea
Dmitry Shaburov <shaburov.d@skbkontur.ru>
2016-10-13 17:07:24 +0500

    Modified deploy script to work with .NET Core

--- Deploy/deploy.ps1
+++ Deploy/deploy.ps1
@@ -10,53 +10,63 @@ properties {
 
 task default -depends SetAppOnline
 
-task SetAppOffline {
- 	if (Test-Path $install_path -pathType container)
-	{
-		Copy-Item "$base_dir\App_Offline.htm" $install_path -Force
-		Copy-Item "$base_dir\web.config" $install_path -Force
-		if (Test-Path "$install_path\global.asax") { Remove-Item "$install_path\global.asax" -Force }
-		Start-Sleep -s 10
-	}
+task Build {
+    exec { dotnet build "$base_dir\..\WebApplication" -c Release }
+}
+
+task SetAppOffline -depends Build {
+    if (Test-Path $install_path -pathType container)
+    {
+        Copy-Item "$base_dir\App_Offline.htm" $install_path -Force
+        Copy-Item "$base_dir\web.config" $install_path -Force
+        if (Test-Path "$install_path\global.asax") { Remove-Item "$install_path\global.asax" -Force }
+        Start-Sleep -s 10
+    }
 }
 
 task Clean -depends SetAppOffline {
- 	if (Test-Path $install_path -pathType container)
-	{
-		Remove-Item "$install_path\*" -Exclude "App_Offline.htm","web.config" -Force -Recurse
-		Start-Sleep -s 10
-	}
+    if (Test-Path $install_path -pathType container)
+    {
+        Remove-Item "$install_path\*" -Exclude "App_Offline.htm","web.config" -Force -Recurse
+        Start-Sleep -s 10
+    }
 }
 
 task CopyContent -depends Clean {
- 	if (!(Test-Path $install_path -pathType container))
-	{
-		New-Item $install_path -Type Directory
-	}
-	Copy-Item "$base_dir\..\WebApplication\*" $install_path -Recurse -Force
+    if (!(Test-Path $install_path -pathType container))
+    {
+        New-Item $install_path -Type Directory
+    }
+    Copy-Item "$base_dir\..\WebApplication\bin\Release\net461\*" "$install_path\bin" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\Content" "$install_path\Content" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\fonts" "$install_path\fonts" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\Scripts" "$install_path\Scripts" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\Views" "$install_path\Views" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\Global.asax" "$install_path\Global.asax" -Recurse -Force
+    Copy-Item "$base_dir\..\WebApplication\Web.config" "$install_path\Web.config" -Recurse -Force
 }
 
 task CreateWebsite -depends CopyContent {
-  	[Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration')
-	$server_manager = [Microsoft.Web.Administration.ServerManager]::OpenRemote($remote_server)
-	if (!$server_manager.ApplicationPools[$iis_app_pool_name])
-	{
-		$iis_app_pool = $server_manager.ApplicationPools.Add($iis_app_pool_name)
-		$iis_app_pool.ManagedRuntimeVersion = $iis_app_pool_dotnet
-		$server_manager.CommitChanges()
-	}
-	if (!$server_manager.Sites[$iis_site_name])
-	{
-		$iis_site = $server_manager.Sites.Add($iis_site_name, $install_path_local,  80)
-		$iis_site.ServerAutoStart = True
-		$iis_site.Start()
-		$server_manager.CommitChanges()
-	}
+    [Reflection.Assembly]::LoadWithPartialName('Microsoft.Web.Administration')
+    $server_manager = [Microsoft.Web.Administration.ServerManager]::OpenRemote($remote_server)
+    if (!$server_manager.ApplicationPools[$iis_app_pool_name])
+    {
+        $iis_app_pool = $server_manager.ApplicationPools.Add($iis_app_pool_name)
+        $iis_app_pool.ManagedRuntimeVersion = $iis_app_pool_dotnet
+        $server_manager.CommitChanges()
+        }
+    if (!$server_manager.Sites[$iis_site_name])
+    {
+        $iis_site = $server_manager.Sites.Add($iis_site_name, $install_path_local,  80)
+        $iis_site.ServerAutoStart = True
+        $iis_site.Start()
+        $server_manager.CommitChanges()
+    }
 }
-	
+
 task SetAppOnline -depends CreateWebsite {
- 	if (Test-Path "$install_path\App_Offline.htm")
-	{
-		Remove-Item "$install_path\App_Offline.htm" -Force
-	}
+    if (Test-Path "$install_path\App_Offline.htm")
+    {
+        Remove-Item "$install_path\App_Offline.htm" -Force
+    }
 }
