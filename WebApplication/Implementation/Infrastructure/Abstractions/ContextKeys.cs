99aaa3c18952c9d0cfba52647a07265c614188aa
Lesha Khvorost <hvorost@skbkontur.ru>
2016-12-18 21:00:28 +0500

    WebApplication delete unused code

--- WebApplication/Implementation/Infrastructure/Abstractions/ContextKeys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SKBKontur.Treller.WebApplication.Implementation.Infrastructure.Abstractions
-{
-    public class ContextKeys
-    {
-        public const string TasksKey = "Tasks";
-        public const string TaskDetalizationKey = "TaskDetalization";
-    }
-}
--- WebApplication/Implementation/Infrastructure/Credentials/IStaffAdCredentialService.cs
+++
@@ -1,9 +0,0 @@
-﻿using SKBKontur.TaskManagerClient.CredentialServiceAbstractions;
-
-namespace SKBKontur.Treller.WebApplication.Implementation.Infrastructure.Credentials
-{
-    public interface IStaffAdCredentialService
-    {
-        DomainCredentials GetStaffCredentials();
-    }
-}
