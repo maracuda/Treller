005b51d3bbe29e9bb3c32fb785931e892c19ed74
Lesha Khvorost <hvorost@skbkontur.ru>
2016-12-27 07:49:22 +0500

    bufix Logger implement ILogService

---
+++ Logger/LogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SKBKontur.Treller.Logger
+{
+    public class LogService : ILogService
+    {
+        public event EventHandler<ErrorEventArgs> OnError;
+        public void RegisterError(string message)
+        {
+            OnError?.Invoke(this, new ErrorEventArgs {Message = message});
+        }
+
+        public void RegisterError(string message, Exception e)
+        {
+            OnError?.Invoke(this, new ErrorEventArgs {Message = message, Exception = e});
+        }
+    }
+}
