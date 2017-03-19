eac83293a0c0fa789184cd9d2d1fe5332b83d916
Ivan Sosnin <isosnin@skbkontur.ru>
2017-03-19 15:46:19 +0500

    Webpack for views

---
+++ WebApplication/Implementation/Webpack/WebpackEntries.cs
@@ -0,0 +1,10 @@
+namespace SKBKontur.Treller.WebApplication.Implementation.Webpack
+{
+    public class WebpackEntries
+    {
+        public const string Manifest = "manifest";
+        public const string Vendor = "vendor";
+
+        public const string News = "news";
+    }
+}
