9e67f1f670d62cb06e8f8e4914532d3301d9e3a0
Ivan Sosnin <isosnin@skbkontur.ru>
2017-03-25 15:40:25 +0500

    Add deploy script

--- frontend_tasks/webpack/webpack.dev.config.js
+++ frontend_tasks/webpack/webpack.dev.config.js
@@ -2,53 +2,55 @@ const path = require("path");
 const webpack = require("webpack");
 const extractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                loader: extractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader?sourceMap&localIdentName=[name]-[local]-[hash:base64:8]"
-                })
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: "expanded",
-                            sourceMap: true,
-                            sourceMapContents: true
+module.exports = function(webAppRoot) {
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    loader: extractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader?sourceMap&localIdentName=[name]-[local]-[hash:base64:8]"
+                    })
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                outputStyle: "expanded",
+                                sourceMap: true,
+                                sourceMapContents: true
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: () => ([
+                                    require("autoprefixer")
+                                ])
+                            }
                         }
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            plugins: () => ([
-                                require("autoprefixer")
-                            ])
-                        }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        output: {
+            path: path.join(webAppRoot, "Content", "bundle")
+        },
+        plugins: [
+            // new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin(),
+            new extractTextPlugin({
+                filename: "[name].css",
+                allChunks: true
+            })
+            // new webpack.DefinePlugin({
+            //     "process.env": {
+            //         BUILD_TARGET: JSON.stringify("client")
+            //     }
+            // })
         ]
-    },
-    output: {
-        path: path.join("C:\\Treller", "Content", "bundle")
-    },
-    plugins: [
-        // new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new extractTextPlugin({
-            filename: "[name].css",
-            allChunks: true
-        })
-        // new webpack.DefinePlugin({
-        //     "process.env": {
-        //         BUILD_TARGET: JSON.stringify("client")
-        //     }
-        // })
-    ]
+    };
 };
