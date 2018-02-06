0b25d3f651f81aaafddbee4a2014b8413089b286
tolltech <alexandrovpe@gmail.com>
2018-02-06 13:35:59 +0400

    roslyn doesnt work

---
+++ ProjectMap/DboWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
+using Tolltech.TollEnnobler.SolutionFixers;
+
+namespace SKBKontur.Tolltech.ProjectMap
+{
+    public class DboWriter : IFixer
+    {
+        public void Fix(Document document, DocumentEditor documentEditor)
+        {
+            Console.WriteLine(document.Name);
+        }
+
+        public string Name => "DboWriter";
+        public int Order => 0;
+    }
+}
---
+++ ProjectMap/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tolltech.TollEnnobler;
+
+namespace SKBKontur.Tolltech.ProjectMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fixerRunner = new FixerRunner();
+
+            fixerRunner.Run(new Settings
+            {
+                Log4NetFileName = "",
+                ProjectNameFilter = x => x.Contains("BillingService"),
+                RootNamespaceForNinjectConfiguring = "SKBKontur.Tolltech",
+                SolutionPath = "D:/billy/Billing/Services/BillingServices.sln"
+            });
+        }
+    }
+}
