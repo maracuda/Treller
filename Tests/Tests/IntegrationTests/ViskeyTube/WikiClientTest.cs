86c7c0d135a5dfaa8a20a49c6c20904fdffb8e4b
tolltech <alexandrovpe@gmail.com>
2018-02-11 18:49:41 +0400

    try wiki

---
+++ Tests/Tests/IntegrationTests/ViskeyTube/WikiClientTest.cs
@@ -0,0 +1,15 @@
+﻿using ViskeyTube.Wiki;
+using Xunit;
+
+namespace Tests.Tests.IntegrationTests.ViskeyTube
+{
+    public class WikiClientTest
+    {
+        [Fact]
+        public void AbleToWiki()
+        {
+            var wikiClient = new WikiClient();
+            var s = wikiClient.GetPage();
+        }
+    }
+}
---
+++ ViskeyTube/Wiki/IwikiClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace ViskeyTube.Wiki
+{
+    public interface IWikiClient
+    {
+        string GetPage();
+    }
+
+    public class WikiClient : IWikiClient
+    {
+        public string GetPage()
+        {
+            using (var client = new WebClient())
+            {
+                var s = client.DownloadString("00");
+                s = client.DownloadString("http://wiki.skbkontur.ru/rest/api/content?type=blogpost&spaceKey=TST&title=Команда");
+                return s;
+            }
+        }
+    }
+}
