edc393ac5c4999f7add3418db773154d5bf52525
tolltech <alexandrovpe@gmail.com>
2018-02-19 19:30:38 +0400

    parse date from russian string

---
+++ Tests/Tests/IntegrationTests/ViskeyTube/DateTimeHelpersTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ViskeyTube.Common;
+using Xunit;
+
+namespace Tests.Tests.IntegrationTests.ViskeyTube
+{
+    public class DateTimeHelpersTest
+    {
+        [Theory]
+        [InlineData("2017-01-24", "Бла бла бла 24 января 2017 г.")]
+        [InlineData("2017-02-24", "Бла бла бла 24 февраля 2017 г.")]
+        [InlineData("2017-03-24", "Бла бла бла 24 марта 2017 г.")]
+        [InlineData("2017-04-24", "Бла бла бла 24 апреля 2017 г.")]
+        [InlineData("2017-05-24", "Бла бла бла 24 мая 2017 г.")]
+        [InlineData("2017-06-24", "Бла бла бла 24 июня 2017 г.")]
+        [InlineData("2017-07-24", "Бла бла бла 24 июля 2017 г.")]
+        [InlineData("2017-08-24", "Бла бла бла 24 августа 2017 г.")]
+        [InlineData("2017-09-24", "Бла бла бла 24 сентября 2017 г.")]
+        [InlineData("2017-10-24", "Бла бла бла 24 октября 2017 г.")]
+        [InlineData("2017-11-24", "Бла бла бла 24 ноября 2017 г.")]
+        [InlineData("2017-12-24", "Бла бла бла 24 декабря 2017 г.")]
+        public void AbleToParseDateTime(string expected, string source)
+        {
+            var actual = DateTimeHelpers.ExtractRussianDateTime(source);
+            Assert.Equal(DateTime.Parse(expected), actual);
+        }
+    }
+}
---
+++ ViskeyTube/Common/DateTimeHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ViskeyTube.Common
+{
+    public static class DateTimeHelpers
+    {
+        private static Dictionary<string, string> russianEnglishMonthMap = new Dictionary<string, string>
+        {
+            { "января" , "Jan"},
+            { "февраля","Feb"},
+            { "марта","Mar"},
+            { "апреля", "Apr"},
+            { "мая","May"},
+            { "июня","Jun"},
+            { "июля","Jul"},
+            { "августа","Aug"},
+            { "сентября","Sep"},
+            { "октября","Oct"},
+            { "ноября" ,"Nov"},
+            { "декабря","Dec"},
+        };
+
+        private static readonly Regex regexForRUssianDateTime = new Regex(@"(([0-9])|([0-2][0-9])|([3][0-1]))\ ("
+                                                                       + "января" + "|"
+                                                                       + "февраля" + "|"
+                                                                       + "марта" + "|"
+                                                                       + "апреля" + "|"
+                                                                       + "мая" + "|"
+                                                                       + "июня" + "|"
+                                                                       + "июля" + "|"
+                                                                       + "августа" + "|"
+                                                                       + "сентября" + "|"
+                                                                       + "октября" + "|"
+                                                                       + "ноября" + "|"
+                                                                       + "декабря"
+                                                                       + @")\ \d{4}");
+
+        public static DateTime? ExtractRussianDateTime(string source)
+        {
+            var match = regexForRUssianDateTime.Match(source);
+            if (!match.Success)
+                return null;
+
+            var extractedRussian = match.Value;
+            var extractedEnglish = extractedRussian.Replace(' ', '-');
+            foreach (var kvp in russianEnglishMonthMap)
+            {
+                extractedEnglish = extractedEnglish.Replace(kvp.Key, kvp.Value);
+            }
+
+            return DateTime.TryParse(extractedEnglish, out var result) ? result : (DateTime?)null;
+        }
+    }
+}
