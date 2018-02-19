dd42e0b9089427c19085e89a9afee7c681151389
tolltech <alexandrovpe@gmail.com>
2018-02-19 19:31:26 +0400

    start to fill description from wiki

--- ViskeyTube/CloudShare/VideoToUploadProvider.cs
+++ ViskeyTube/CloudShare/VideoToUploadProvider.cs
@@ -1,15 +1,37 @@
-﻿namespace ViskeyTube.CloudShare
+﻿using System;
+using System.Text.RegularExpressions;
+using ViskeyTube.Common;
+using ViskeyTube.Wiki;
+
+namespace ViskeyTube.CloudShare
 {
     public class VideoToUploadProvider : IVideoToUploadProvider
     {
+        private const string ArchieveWhiskeyPageId = "156696999";
+
+        private readonly IWikiClient wikiClient;
+
+        public VideoToUploadProvider(IWikiClient wikiClient)
+        {
+            this.wikiClient = wikiClient;
+        }
+
+       
+
         public VideoToUpload GetVideoToUpload(DriveFile driveFile)
         {
-            //todo: запарсить вики        
-            return new VideoToUpload
+            var videoToUpload = new VideoToUpload
             {
                 Title = driveFile.Name,
-                Description = $"Night Whiskey of {driveFile.CreatedTime:d}"
+                Description = $"Night Whiskey"
             };
+
+            var date = DateTimeHelpers.ExtractRussianDateTime(videoToUpload.Title);
+
+            var whiskeyPages = wikiClient.GetChildren(ArchieveWhiskeyPageId);
+
+
+            return videoToUpload;
         }
     }
 }
